23ab2931c90111b20257a5ebadba309690b7795d
Himanshu-Lilhore <himanshulilhore@gmail.com>
2026-02-20 14:19:36 +0530

    protected routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './registerUser.dto';
 import { LoginDto } from './loginUser.dto';
+import { AuthGuard } from './auth.guard';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('register')
   register(@Body() registerUserDto: RegisterDto) {
@@ -16,5 +28,19 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginDto) {
     return this.authService.loginUser(loginUserDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    const userId = req.user.sub;
+    const user = await this.userService.getUserById(userId);
+    return {
+      id: user?.id,
+      fname: user?.fname,
+      lname: user?.lname,
+      email: user?.email,
+      role: user?.role,
+    };
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -32,4 +32,8 @@ export class UserService {
   async findByEmail(email: string) {
     return this.userModel.findOne({ email });
   }
+
+  async getUserById(id: string) {
+    return this.userModel.findById(id);
+  }
 }
